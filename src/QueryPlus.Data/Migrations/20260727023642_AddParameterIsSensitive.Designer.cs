2b8083ac5658d8eab375e8faae55f30dbb498e93
tiglate <128345445+tiglate@users.noreply.github.com>
2026-07-26 23:37:17 -0300

    fix(data): add EF Core migration for is_sensitive column on tb_procedure_parameter

--- src/QueryPlus.Data/Migrations/20260727023642_AddParameterIsSensitive.Designer.cs
+++ src/QueryPlus.Data/Migrations/20260727023642_AddParameterIsSensitive.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QueryPlus.Data.Context;
 
@@ -11,9 +12,11 @@ using QueryPlus.Data.Context;
 namespace QueryPlus.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260727023642_AddParameterIsSensitive")]
+    partial class AddParameterIsSensitive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
